fc5f59bd06993960785154d8db302be563325c49
pinguet62 <pinguet62@gmail.com>
2014-08-23 00:54:44 +0200

    Creation

---
+++ webapp/src/main/java/fr/pinguet62/dictionary/dao/DescriptionDao.java
@@ -0,0 +1,10 @@
+package fr.pinguet62.dictionary.dao;
+
+import org.springframework.stereotype.Repository;
+
+import fr.pinguet62.dictionary.model.Description;
+
+/** The DAO for {@link Description}. */
+@Repository
+public final class DescriptionDao extends AbstractDao<Description, Integer> {
+}
---
+++ webapp/src/main/java/fr/pinguet62/dictionary/dao/KeywordDao.java
@@ -0,0 +1,10 @@
+package fr.pinguet62.dictionary.dao;
+
+import org.springframework.stereotype.Repository;
+
+import fr.pinguet62.dictionary.model.Keyword;
+
+/** The DAO for {@link Keyword}. */
+@Repository
+public final class KeywordDao extends AbstractDao<Keyword, Integer> {
+}
