1b950781b58088361c5809a16019741e31ef9828
pinguet62 <pinguet62@gmail.com>
2014-08-30 15:44:53 +0200

    Filter for pagination

--- webapp/src/main/java/fr/pinguet62/dictionary/filter/PaginationFilter.java
+++ webapp/src/main/java/fr/pinguet62/dictionary/filter/PaginationFilter.java
@@ -4,72 +4,61 @@ package fr.pinguet62.dictionary.filter;
 // TODO null values tests
 public final class PaginationFilter {
 
+    /**
+     * The index of first result.
+     * <p>
+     * The minimum is {@code 0} and can't be negative.
+     */
+    private Integer first;
+
     /** The number of result per page. */
     private Integer numberPerPage;
 
-    /**
-     * The current page.
-     * <p>
-     * The first page is {@code 1} and can't be negative.
-     */
-    private Integer page;
-
-    /**
-     * Check if the two parameters {@link #page} and {@link #numberPerPage} are
-     * set.
-     *
-     * @throws FilterException
-     *             If {@link #page} or {@link #numberPerPage} is not set.
-     */
-    private void checkParameters() {
-        if (page == null)
-            throw new FilterException("The page is not set.");
-        if (numberPerPage == null)
-            throw new FilterException("The number per page is not set.");
+    public Integer getFirst() {
+        return first;
     }
 
     /**
      * Get the index of first result.
-     * <p>
-     * Calculated from {@link #page} and {@link #numberPerPage}.
      *
      * @return The first index.
-     * @throws FilterException
-     *             If {@link #page} or {@link #numberPerPage} is not set.
      */
-    public int getFirstIndex() {
-        checkParameters();
-        return (page - 1) * numberPerPage;
-    }
-
-    /**
-     * Get the index of last result.
-     * <p>
-     * Calculated from {@link #page} and {@link #numberPerPage}.
-     *
-     * @return The last index.
-     * @throws FilterException
-     *             If {@link #page} or {@link #numberPerPage} is not set.
-     */
-    public int getLastIndex() {
-        checkParameters();
-        return page * numberPerPage;
+    public Integer getFirstIndex() {
+        return first;
     }
 
     public Integer getNumberPerPage() {
         return numberPerPage;
     }
 
-    public Integer getPage() {
-        return page;
+    /**
+     * Set the first index..
+     *
+     * @param first
+     *            The first index.
+     * @throws IllegalArgumentException
+     *             Negative value.
+     */
+    public void setFirst(Integer first) {
+        if ((first != null) && (first < 0))
+            throw new IllegalArgumentException("The index can't be negative.");
+
+        this.first = first;
     }
 
+    /**
+     * Set the number of results per page.
+     *
+     * @param numberPerPage
+     *            The number of results per page.
+     * @throws IllegalArgumentException
+     *             Negative value.
+     */
     public void setNumberPerPage(Integer numberPerPage) {
-        this.numberPerPage = numberPerPage;
-    }
+        if ((numberPerPage != null) && (numberPerPage <= 0))
+            throw new IllegalArgumentException("The number must be positive.");
 
-    public void setPage(Integer page) {
-        this.page = page;
+        this.numberPerPage = numberPerPage;
     }
 
 }
--- webapp/src/main/java/fr/pinguet62/dictionary/filter/PaginationResult.java
+++ webapp/src/main/java/fr/pinguet62/dictionary/filter/PaginationResult.java
@@ -6,6 +6,9 @@ public final class PaginationResult<T> {
 
     private final List<T> items;
 
+    /** The number of result per page. */
+    private final Integer numberPerPage = 5; // TODO delete me
+
     private final long totalCount;
 
     public PaginationResult(List<T> items, long totalCount) {
@@ -15,6 +18,10 @@ public final class PaginationResult<T> {
 
     public List<T> getItems() {
         return items;
+    }
+
+    public Integer getNumberPerPage() {
+        return numberPerPage;
     }
 
     public long getTotalCount() {
