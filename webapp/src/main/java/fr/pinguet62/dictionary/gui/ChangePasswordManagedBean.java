a1527401230fc895e1dde91f3f3a6eafa3fd416c
pinguet62 <pinguet62@gmail.com>
2014-10-23 22:52:25 +0200

    Change password

---
+++ webapp/src/main/java/fr/pinguet62/dictionary/gui/ChangePasswordManagedBean.java
@@ -0,0 +1,44 @@
+package fr.pinguet62.dictionary.gui;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public final class ChangePasswordManagedBean {
+
+    private String newPassword;
+
+    private String newPasswordConfirmation;
+
+    private String oldPassword;
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public String getNewPasswordConfirmation() {
+        return newPasswordConfirmation;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public void setNewPasswordConfirmation(String newPasswordConfirmation) {
+        this.newPasswordConfirmation = newPasswordConfirmation;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public void submit() {
+        System.out.println("TODO");
+    }
+
+}
