a19c859764e61e688e347f567322ea8b2d0c51d4
Pinguet62 <pinguet62@gmail.com>
2015-06-24 13:39:18 +0200

    Theme Switcher Configuration and implementation

---
+++ src/main/java/fr/pinguet62/jsfring/gui/theme/ThemeSwitcherManagedBean.java
@@ -0,0 +1,39 @@
+package fr.pinguet62.jsfring.gui.theme;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/** Used to switch of PrimeFaces {@link Theme}. */
+@ManagedBean(name = "themeSwitcherManagedBean")
+@SessionScoped
+public final class ThemeSwitcherManagedBean implements Serializable {
+
+    private static final long serialVersionUID = 1;
+
+    /** The current {@link Theme}. */
+    private Theme theme = Theme.fromKey("dark-hive");
+
+    /** Get the current {@link Theme}. */
+    public Theme getTheme() {
+        return theme;
+    }
+
+    /** @return {@link Theme#values()} */
+    public Theme[] getThemes() {
+        return Theme.values();
+    }
+
+    /**
+     * Set the new current {@link Theme}.
+     *
+     * @throws NullPointerException If the parameter is {@code null}.
+     */
+    public void setTheme(Theme theme) {
+        Objects.requireNonNull(theme);
+        this.theme = theme;
+    }
+
+}
