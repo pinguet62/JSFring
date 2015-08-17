37ae48ee393486692125cb039081a2086bb0c546
Pinguet62 <pinguet62@gmail.com>
2015-08-17 23:05:08 +0200

    Cache - Fixed #6 * Config * Services : Right readonly, Profile read/write

--- src/main/java/fr/pinguet62/jsfring/service/RightService.java
+++ src/main/java/fr/pinguet62/jsfring/service/RightService.java
@@ -1,7 +1,12 @@
 package fr.pinguet62.jsfring.service;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.pinguet62.jsfring.dao.RightDao;
 import fr.pinguet62.jsfring.model.Right;
@@ -10,9 +15,25 @@ import fr.pinguet62.jsfring.model.Right;
 @Named
 public class RightService extends AbstractService<Right, String> {
 
+    private static final String CACHE = "right";
+
     @Inject
     protected RightService(RightDao dao) {
         super(dao);
+    }
+
+    @Override
+    @Cacheable(CACHE)
+    @Transactional(readOnly = true)
+    public Right get(String id) {
+        return super.get(id);
+    }
+
+    @Override
+    @Cacheable(CACHE)
+    @Transactional(readOnly = true)
+    public List<Right> getAll() {
+        return super.getAll();
     }
 
 }
