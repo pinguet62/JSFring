207b22c940bbef4c37906d99a4306fdef8763386
Pinguet62 <pinguet62@gmail.com>
2015-08-19 23:23:30 +0200

    Spring Security: config to get current authenticated user

--- src/main/java/fr/pinguet62/jsfring/gui/sample/UserBean.java
+++ src/main/java/fr/pinguet62/jsfring/gui/sample/UserBean.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import fr.pinguet62.jsfring.model.User;
+import fr.pinguet62.jsfring.gui.login.springsecurity.UserDetailsImpl;
 
 @Named
 @SessionScoped
@@ -17,28 +20,19 @@ public final class UserBean implements Serializable {
     private static final long serialVersionUID = 1;
 
     /**
-     * Get the user in session.
+     * Get the {@link UserDetails} in {@link SecurityContext}.
      *
-     * @return The session user.
-     * @see SecurityContextHolder
+     * @return The {@link UserDetails}.
+     * @see UserDetailsImpl
      */
-    public static User get() {
+    public static UserDetails get() {
         Authentication authentication = SecurityContextHolder.getContext()
                 .getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated())
+        if (authentication == null || !authentication.isAuthenticated()
+                || authentication instanceof AnonymousAuthenticationToken)
             return null;
         else
-            return (User) SecurityContextHolder.getContext()
-                    .getAuthentication().getDetails();
-    }
-
-    /**  */
-    public User getDao() {
-        return UserBean.get();
-    }
-
-    public String getEmail() {
-        return "mylogin";
+            return (UserDetails) authentication.getPrincipal();
     }
 
 }
