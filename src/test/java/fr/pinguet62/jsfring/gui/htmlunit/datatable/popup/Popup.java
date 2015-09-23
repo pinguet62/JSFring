55f1e05d0cc77cf756f0381e106f4ea201e0bce3
Pinguet62 <pinguet62@gmail.com>
2015-09-23 13:42:30 +0200

    HtmlUnit popup debug action

--- src/test/java/fr/pinguet62/jsfring/gui/htmlunit/datatable/popup/Popup.java
+++ src/test/java/fr/pinguet62/jsfring/gui/htmlunit/datatable/popup/Popup.java
@@ -6,6 +6,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+import fr.pinguet62.jsfring.gui.htmlunit.AbstractPage;
 import fr.pinguet62.jsfring.gui.htmlunit.NavigatorException;
 
 public interface Popup {
@@ -14,8 +15,8 @@ public interface Popup {
         HtmlAnchor x = (HtmlAnchor) getDialog().getByXPath(
                 "./div[contains(@class, 'ui-dialog-titlebar')]/a[contains(@class, 'ui-dialog-titlebar-close')]").get(0);
         try {
-            x.click();
-            // TODO debug();
+            HtmlPage page = x.click();
+            AbstractPage.debug(page); // TODO debug();
         } catch (IOException e) {
             throw new NavigatorException(e);
         }
--- src/test/java/fr/pinguet62/jsfring/gui/htmlunit/datatable/popup/SubmitPopup.java
+++ src/test/java/fr/pinguet62/jsfring/gui/htmlunit/datatable/popup/SubmitPopup.java
@@ -3,19 +3,21 @@ package fr.pinguet62.jsfring.gui.htmlunit.datatable.popup;
 import java.io.IOException;
 
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+import fr.pinguet62.jsfring.gui.htmlunit.AbstractPage;
 import fr.pinguet62.jsfring.gui.htmlunit.NavigatorException;
 
 /** {@link Popup} with "Submit" button. */
 public interface SubmitPopup extends Popup {
 
     default void submit() {
-        HtmlButton button = (HtmlButton) getDialog().getByXPath("./div[contains(@class, 'ui-dialog-content')]/form/button")
+        HtmlButton submit = (HtmlButton) getDialog().getByXPath("./div[contains(@class, 'ui-dialog-content')]/form/button")
                 .get(0);
         try {
-            button.click();
+            HtmlPage page = submit.click();
             waitJS();
-            // TODO debug();
+            AbstractPage.debug(page); // TODO debug();
         } catch (IOException e) {
             throw new NavigatorException(e);
         }
