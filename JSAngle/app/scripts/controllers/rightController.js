d7918edc9f6c3092832e362abd511743d1028003
Pinguet62 <pinguet62@gmail.com>
2015-12-10 13:49:22 +0100

    CRUD for user/profile/right: view/controller/service

--- JSAngle/app/scripts/controllers/rightController.js
+++ JSAngle/app/scripts/controllers/rightController.js
@@ -9,6 +9,62 @@
 	 * Controller of the jsangleApp
 	 */
 	angular.module('jsangleApp')
-		.controller('rightController', function () {
-		});
+		.controller('rightController', ['$scope', 'rightService', function($scope, rightService) {
+			
+			var lazy = false;
+			
+			var paginationOptions = {
+				pageNumber: 1,
+				pageSize: 2,
+				sort: null,
+				filter: null
+			};
+			
+			/**
+			 * Executed after: pagination, sorting or filtering
+			 * Call the corresponding service.
+			 */
+			var getPage = function() {
+				if (lazy) {
+					var results = rightService.find(paginationOptions);
+					$scope.gridOptions.data = results.values;
+					$scope.gridOptions.totalItems = results.totalCount;
+				} else {
+					rightService.list(function(results) {
+							$scope.gridOptions.data = results;
+							$scope.gridOptions.totalItems = results.length;
+						});
+				}
+			}
+			
+			$scope.gridOptions = {
+				// Pagination
+				paginationPageSizes: [2, 5, 10],
+				paginationPageSize: paginationOptions.pageSize,
+				// Lazy-loading
+				useExternalPagination: true,
+				onRegisterApi: function(gridApi) {
+					// Pagination
+					gridApi.pagination.on.paginationChanged($scope, function(newPage, pageSize) {
+						paginationOptions.pageNumber = newPage;
+						paginationOptions.pageSize = pageSize;
+						getPage();
+					});
+					// Sorting
+					gridApi.core.on.sortChanged($scope, function(grid, sortColumns) {
+						if (sortColumns.length === 0)
+							paginationOptions.sort = null;
+						else
+							paginationOptions.sort = {
+								name: sortColumns[0].name,
+								order: sortColumns[0].sort.direction
+							};
+						getPage();
+					});
+					// TODO Filtering
+				}
+			};
+			
+			getPage(); // initial load
+		}]);
 })();
--- JSAngle/app/scripts/services/rightService.js
+++ JSAngle/app/scripts/services/rightService.js
@@ -9,6 +9,58 @@
 	 * Service in the jsangleApp.
 	 */
 	angular.module('jsangleApp')
-		.service('rightService', function () {
+		.service('rightService', function($http) {
+			var ms = this;
+			
+			ms.list = function(callback) {
+				$http
+					.get("http://localhost:8080/webservice/rest/right/")
+					.success(function(response) {
+						callback && callback(response);
+					});
+			};
+			
+			ms.listMocked = function() {
+				return [
+					{code: "code1", title: "title 9-1"},
+					{code: "code2", title: "title 8-2"},
+					{code: "code3", title: "title 7-3"},
+					{code: "code4", title: "title 6-4"},
+					{code: "code5", title: "title 5-5"},
+					{code: "code6", title: "title 4-6"},
+					{code: "code7", title: "title 3-7"},
+					{code: "code8", title: "title 2-8"},
+					{code: "code9", title: "title 1-9"}
+				];
+			};
+			
+			ms.findMocked = function(paginationOptions) {
+				var totalResults = ms.list();
+				// Filter
+				var filteredResults = totalResults;
+				// Sort
+				var sortedResults = filteredResults;
+				if (paginationOptions.sort != null) {
+					// Order
+					var order = paginationOptions.sort.order === 'asc' ? 1 : -1;
+					// Field
+					var comparator;
+					if (paginationOptions.sort.name == 'code')
+						comparator = function(a, b) { return order * a.code.localeCompare(b.code); };
+					else if (paginationOptions.sort.name == 'title')
+						comparator = function(a, b) { return order * a.title.localeCompare(b.title); };
+					// Apply
+					sortedResults = sortedResults.sort(comparator);
+				}
+				// Pagination
+				var first = paginationOptions.pageSize * (paginationOptions.pageNumber - 1);
+				var last = paginationOptions.pageSize * paginationOptions.pageNumber;
+				var results = sortedResults.slice(first, last);
+				// Result
+				return {
+					'values': results,
+					'totalCount': filteredResults.length
+				};
+			}
 		});
 })();
