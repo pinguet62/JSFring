80b7d19dc5cff4b700a3f685a382f331dd5a2a62
Pinguet62 <pinguet62@gmail.com>
2015-12-14 22:11:58 +0100

    AngularJS: i18n

---
+++ JSAngle/app/scripts/i18n.js
@@ -0,0 +1,29 @@
+(function() {
+	'use strict';
+
+	/**
+	 * Configuration of i18n translations.
+	 */
+	angular.module('jsangleApp')
+		.config(['$translateProvider', function($translateProvider) {
+			$translateProvider.translations('en', {
+				'right.code': 'Code',
+				'right.title': 'Title',
+				'profile.id': 'Id',
+				'profile.title': 'Title',
+				'user.login': 'Login',
+				'user.email': 'Email',
+				'user.lastConnection': 'Last connection',
+			});
+			$translateProvider.translations('fr', {
+				'right.code': 'Code',
+				'right.title': 'Titre',
+				'profile.id': 'Id',
+				'profile.title': 'Titre',
+				'user.login': 'Identifiant',
+				'user.email': 'Email',
+				'user.lastConnection': 'Dernière connexion',
+			});
+			$translateProvider.preferredLanguage('en');
+		}]);
+})();
