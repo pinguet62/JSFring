15e30a6cb8529b36748c8aea7402c664ab4e8808
Pinguet62 <pinguet62@gmail.com>
2015-12-20 22:28:40 +0100

    Use picklist for profile update

--- JSAngle/app/scripts/controllers/profileController.js
+++ JSAngle/app/scripts/controllers/profileController.js
@@ -9,28 +9,8 @@
 	 * Controller of the jsangleApp
 	 */
 	angular.module('jsangleApp')
-		.controller('profileController', ['$scope', 'profileService', '$translate', 'ngDialog', function($scope, profileService, $translate, ngDialog) {
+		.controller('profileController', ['$scope', 'profileService', '$translate', 'ngDialog', 'rightService', function($scope, profileService, $translate, ngDialog, rightService) {
 			initCrudController($scope, profileService);
-			
-			$scope.openShowDialog = function(entity) {
-				ngDialog.open({
-					template: 'views/profile/show.html',
-					controller: ['$scope', function($scope) {
-						$scope.profile = entity;
-					}]
-				});
-			};
-			$scope.openUpdateDialog = function(entity) {
-				ngDialog.open({
-					template: 'views/profile/update.html',
-					controller: ['$scope', function($scope) {
-						$scope.profile = entity;
-					}]
-				});
-			};
-			// $scope.openDeleteDialog = function() {
-				// ngDialog.open({ template: 'views/profile/delete.html' });
-			// };
 			
 			// Column titles
 			$scope.gridOptions.columnDefs = [
@@ -42,5 +22,57 @@
 						'<button id="update" type="button" ng-click="grid.appScope.openUpdateDialog(row.entity)">'+$translate.instant('grid.actions.update')+'</button>' /*+ 
 						'<button id="delete" type="button" ng-click="grid.appScope.openDeleteDialog(row.entity)">'+$translate.instant('grid.actions.delete')+'</button>'*/ },
 			];
+			
+			// TODO synchrone
+			var rights = null;
+			rightService.list(function(results) {
+				rights = results;
+			});
+			
+			// Right association
+			$scope.itemKeyConverter = function(x) { return x.code; };
+			$scope.itemValueConverter = function(x) { return x.title; };
+			$scope.rightsAssociation = {
+				source: [],
+				target: []
+			};
+			$scope.initRightsAssociation = function() {
+				// TODO Synchrone: var rights = rightService.list();
+				for (var i = 0 ; i < rights.length ; i ++)
+					if ($scope.profile.rights.indexOf($scope.itemKeyConverter(rights[i])) === -1)
+						$scope.rightsAssociation.source.push(rights[i]);
+					else
+						$scope.rightsAssociation.target.push(rights[i]);
+			};
+			
+			// Actions
+			$scope.openShowDialog = function(entity) {
+				$scope.profile = entity;
+				$scope.initRightsAssociation(); // Right association
+				ngDialog.open({
+					template: 'views/profile/show.html',
+					scope: $scope
+				});
+			};
+			$scope.openUpdateDialog = function(entity) {
+				$scope.profile = entity;
+				$scope.initRightsAssociation(); // Right association
+				ngDialog.open({
+					template: 'views/profile/update.html',
+					scope: $scope
+				});
+			};
+			// $scope.openDeleteDialog = function() {
+				// ngDialog.open({ template: 'views/profile/delete.html' });
+			// };
+			
+			$scope.update = function() {
+				// Right association
+				$scope.profile.rights = [];
+				for (var i = 0 ; i < rightAssociation.target.length ; i ++)
+					profile.rights.push($scope.itemKeyConverter(rightAssociation.target[i]));
+				
+				profileService.update(profile);
+			};
 		}]);
 })();
