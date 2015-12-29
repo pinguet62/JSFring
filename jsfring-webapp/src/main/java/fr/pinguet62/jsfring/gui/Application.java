f5465c538fdabd4b7afe46158602e42eab05b6bf
Pinguet62 <pinguet62@gmail.com>
2015-12-29 23:53:06 +0100

    Webapp config

--- jsfring-webapp/src/main/java/fr/pinguet62/jsfring/gui/Application.java
+++ jsfring-webapp/src/main/java/fr/pinguet62/jsfring/gui/Application.java
@@ -1,35 +1,12 @@
 package fr.pinguet62.jsfring.gui;
 
-import javax.faces.application.ViewHandler;
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.Servlet;
-import javax.servlet.ServletContext;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.ServletContextAware;
 
-import com.sun.faces.config.ConfigureListener;
-
-//@SpringBootApplication(scanBasePackages = "fr.pinguet62.jsfring")
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = { "", "fr.pinguet62.jsfring" })
-public class Application extends SpringBootServletInitializer implements ServletContextAware {
-
-    public static class Toto extends SpringBootServletInitializer {
-        @Override
-        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-            return application.sources(Application.class, WebConfig.class);
-        }
-    }
+@SpringBootApplication(scanBasePackages = "fr.pinguet62.jsfring")
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -38,38 +15,6 @@ public class Application extends SpringBootServletInitializer implements Servlet
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class, WebConfig.class);
-    }
-
-    /** JSF registration: {@link Servlet} & page extension. */
-    @Bean
-    public ServletRegistrationBean facesServletRegistration() {
-        FacesServlet servlet = new FacesServlet();
-        // TODO check requiered "*"
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*"
-                + ViewHandler.DEFAULT_FACELETS_SUFFIX);
-        servletRegistrationBean.setLoadOnStartup(1);
-        return servletRegistrationBean;
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
-        return new ServletListenerRegistrationBean<ConfigureListener>(new ConfigureListener());
-    }
-
-    /** JSF registration: {@link Servlet} & page extension. */
-    @Bean
-    public ServletRegistrationBean servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
-        // TODO check requiered "*"
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*"
-                + ViewHandler.DEFAULT_FACELETS_SUFFIX);
-        servletRegistrationBean.setLoadOnStartup(1);
-        return servletRegistrationBean;
-    }
-
-    @Override
-    public void setServletContext(ServletContext servletContext) {
-        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
     }
 
 }
--- jsfring-webapp/src/main/java/fr/pinguet62/jsfring/gui/WebConfig.java
+++ jsfring-webapp/src/main/java/fr/pinguet62/jsfring/gui/WebConfig.java
@@ -2,13 +2,22 @@ package fr.pinguet62.jsfring.gui;
 
 import javax.faces.application.ProjectStage;
 import javax.faces.application.ViewHandler;
+import javax.faces.context.FacesContextFactory;
 import javax.faces.convert.Converter;
+import javax.faces.webapp.FacesServlet;
 import javax.inject.Inject;
+import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.primefaces.util.Constants.ContextParams;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.sun.faces.config.ConfigureListener;
 
 import fr.pinguet62.jsfring.gui.theme.ThemeSwitcherBean;
 
@@ -19,15 +28,47 @@ public class WebConfig implements ServletContextInitializer {
     @Inject
     private ThemeSwitcherBean themeSwitcherBean;
 
+    @Bean
+    public FacesServlet facesServlet() {
+        return new FacesServlet();
+    }
+
+    /**
+     * Configure {@link FacesContextFactory} for Spring-Boot.
+     *
+     * @see FacesContextFactory
+     */
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<ConfigureListener>(new ConfigureListener());
+    }
+
+    /** Configure {@code <context-param>} key/values. */
     @Override
     public void onStartup(ServletContext sc) throws ServletException {
         sc.setInitParameter(ProjectStage.PROJECT_STAGE_PARAM_NAME, ProjectStage.Development.toString());
         sc.setInitParameter(ViewHandler.FACELETS_SKIP_COMMENTS_PARAM_NAME, Boolean.TRUE.toString());
         // TimeZone: getDefault() instead of "GMT"
-        sc.setInitParameter(Converter.DATETIMECONVERTER_DEFAULT_TIMEZONE_IS_SYSTEM_TIMEZONE_PARAM_NAME, Boolean.TRUE.toString());
+        sc.setInitParameter(Converter.DATETIMECONVERTER_DEFAULT_TIMEZONE_IS_SYSTEM_TIMEZONE_PARAM_NAME,
+                Boolean.TRUE.toString());
+
+        // JSF without "web.xml" and "faces-config.xml"
+        sc.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+
+        sc.setInitParameter("javax.faces.WEBAPP_RESOURCES_DIRECTORY", "/META-INF/resources");
 
         // Primefaces
-        sc.setInitParameter("primefaces.THEME", themeSwitcherBean.getTheme().getKey());
+        sc.setInitParameter(ContextParams.THEME, themeSwitcherBean.getTheme().getKey());
+    }
+
+    /** JSF registration: {@link Servlet} & page extension. */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        String extension = "*" + ViewHandler.DEFAULT_FACELETS_SUFFIX;
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, extension);
+        servletRegistrationBean.setLoadOnStartup(1);
+        return servletRegistrationBean;
     }
 
 }
