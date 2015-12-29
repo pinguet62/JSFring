6b9ae8838caa9db1e21a220b692a88c5918e478a
Pinguet62 <pinguet62@gmail.com>
2015-12-29 23:52:50 +0100

    Component: rename resource

--- jsfring-webapp/src/main/java/fr/pinguet62/jsfring/gui/component/breadcrumb/BreadCrumbComponent.java
+++ jsfring-webapp/src/main/java/fr/pinguet62/jsfring/gui/component/breadcrumb/BreadCrumbComponent.java
@@ -27,8 +27,8 @@ import org.primefaces.model.menu.MenuModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@FacesComponent(value = "breadCrumb")
-public final class BreadCrumbComponent extends BreadCrumb {
+@FacesComponent(value = "breadCrumb", tagName = "breadCrumb", createTag = true, namespace = "http://java.sun.com/jsf/composite/p62")
+public class BreadCrumbComponent extends BreadCrumb {
 
     private static enum PropertyKeys {
         index, menu;
