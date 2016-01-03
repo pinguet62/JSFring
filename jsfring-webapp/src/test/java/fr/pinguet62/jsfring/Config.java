c30103c697c4e05dc5e4084f4f9530386a55f967
Pinguet62 <pinguet62@gmail.com>
2016-01-03 19:07:45 +0100

    Unittest config: to Spring-Boot

--- jsfring-webapp/src/test/java/fr/pinguet62/jsfring/Config.java
+++ jsfring-webapp/src/test/java/fr/pinguet62/jsfring/Config.java
@@ -4,6 +4,4 @@ public final class Config {
 
     public static final String DATASET = "/dataset.xml";
 
-    public static final String SPRING = "classpath:*/spring/applicationContextTest.xml";
-
 }
