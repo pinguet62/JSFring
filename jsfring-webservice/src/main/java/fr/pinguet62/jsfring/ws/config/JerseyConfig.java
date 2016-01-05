f5ebc3470b46e3fc582647dff0ae931448bef5de
Pinguet62 <pinguet62@gmail.com>
2016-01-06 00:01:43 +0100

    Webservice: Jersey config

--- jsfring-webservice/src/main/java/fr/pinguet62/jsfring/ws/config/JerseyConfig.java
+++ jsfring-webservice/src/main/java/fr/pinguet62/jsfring/ws/config/JerseyConfig.java
@@ -2,13 +2,12 @@ package fr.pinguet62.jsfring.ws.config;
 
 import static org.glassfish.jersey.servlet.ServletProperties.JAXRS_APPLICATION_CLASS;
 
-import java.util.List;
-
-import javax.inject.Inject;
+import javax.annotation.PostConstruct;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -17,22 +16,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JerseyConfig {
 
-    @Configuration
     public static class PackageResourceConfig extends ResourceConfig {
-        @Inject
+        @Autowired
         private BeanFactory beanFactory;
 
-        public PackageResourceConfig() {
-            List<String> packagesList = AutoConfigurationPackages.get(beanFactory);
-            String[] packagesArray = packagesList.toArray(new String[packagesList.size()]);
-            packages(packagesArray);
+        /**
+         * Define the package to scan, who contains JAX-RS annotated classes.
+         * <p>
+         * Use {@link PostConstruct} because {@link #beanFactory} is injected
+         * after {@link PackageResourceConfig#PackageResourceConfig() default
+         * constructor}.
+         */
+        @PostConstruct
+        private void init() {
+            String[] packages = AutoConfigurationPackages.get(beanFactory).stream().toArray(String[]::new);
+            packages(packages);
         }
     }
 
     @Bean
     public ServletRegistrationBean jerseyServlet() {
         ServletRegistrationBean registration = new ServletRegistrationBean(new ServletContainer(), "/*");
-        registration.addInitParameter(JAXRS_APPLICATION_CLASS, JerseyConfig.class.getName());
+        registration.addInitParameter(JAXRS_APPLICATION_CLASS, PackageResourceConfig.class.getName());
         return registration;
     }
 
