8d0b7b5a8d4abb77d2c410e9a7d5676095268dbe
Pinguet62 <pinguet62@gmail.com>
2016-01-08 00:12:06 +0100

    Constant instead of String

--- jsfring-webservice/src/main/java/fr/pinguet62/jsfring/ws/config/AccessControlAllowOriginFilter.java
+++ jsfring-webservice/src/main/java/fr/pinguet62/jsfring/ws/config/AccessControlAllowOriginFilter.java
@@ -1,9 +1,16 @@
 package fr.pinguet62.jsfring.ws.config;
 
+import static java.util.stream.Collectors.joining;
+import static javax.ws.rs.Priorities.HEADER_DECORATOR;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static org.springframework.http.HttpMethod.values;
+
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.annotation.Priority;
-import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -12,18 +19,17 @@ import javax.ws.rs.ext.Provider;
 
 /** Configure the {@code Access-Control-Allow-Origin}. */
 @Provider
-@Priority(Priorities.HEADER_DECORATOR)
+@Priority(HEADER_DECORATOR)
 public class AccessControlAllowOriginFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
         MultivaluedMap<String, Object> headers = response.getHeaders();
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type");
-        // headers.add("Access-Control-Expose-Headers",
-        // "Location, Content-Disposition");
+        headers.add(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        headers.add(ACCESS_CONTROL_ALLOW_HEADERS, "Authorization, Origin, X-Requested-With, Content-Type");
+        // headers.add("Access-Control-Expose-Headers","Location, Content-Disposition");
         // headers.add("Access-Control-Allow-Credentials", true);
-        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, OPTIONS");
+        headers.add(ACCESS_CONTROL_ALLOW_METHODS, Arrays.stream(values()).map(it -> it.toString()).collect(joining(", "))); // TODO Check "*"
     }
 
 }
