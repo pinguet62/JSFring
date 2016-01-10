ec9f90e176fbf55f42d11faa1c25d8fe61e26ee6
Pinguet62 <pinguet62@gmail.com>
2016-01-10 18:40:41 +0100

    Fix Jersey config, for command line running

--- jsfring-webservice/src/main/java/fr/pinguet62/jsfring/ws/config/JerseyConfig.java
+++ jsfring-webservice/src/main/java/fr/pinguet62/jsfring/ws/config/JerseyConfig.java
@@ -1,22 +1,39 @@
 package fr.pinguet62.jsfring.ws.config;
 
+import static java.util.stream.Collectors.joining;
 import static org.glassfish.jersey.servlet.ServletProperties.JAXRS_APPLICATION_CLASS;
 
+import java.util.Collection;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Path;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.util.ClassUtils;
 
 @Configuration
+@ApplicationPath("/")
 public class JerseyConfig {
 
+    /** Register for Jersey. */
     public static class PackageResourceConfig extends ResourceConfig {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(PackageResourceConfig.class);
+
         @Autowired
         private BeanFactory beanFactory;
 
@@ -29,9 +46,30 @@ public class JerseyConfig {
          */
         @PostConstruct
         private void init() {
-            String[] packages = AutoConfigurationPackages.get(beanFactory).stream().toArray(String[]::new);
-            packages(packages);
+            List<String> packages = AutoConfigurationPackages.get(beanFactory);
+            LOGGER.debug("Registrer packages: {}", packages.stream().collect(joining(", ", "[", "]")));
+            registerScannedPackages(packages); // packages(packages.stream().toArray(String[]::new));
         }
+
+        /**
+         * Fix for Jersey package scanning.
+         * <p>
+         * Use Spring to scan packages, finding {@link Path} and setting
+         * directly these classes to Jersey.
+         *
+         * @see <a href="https://java.net/jira/browse/JERSEY-2175">Jersey issue
+         *      JERSEY-2175</a>
+         */
+        private void registerScannedPackages(Collection<String> packages) {
+            ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+            scanner.addIncludeFilter(new AnnotationTypeFilter(Path.class)); // JAX-RS
+            // scanner.addIncludeFilter(new
+            // AnnotationTypeFilter(RestController.class)); // Spring
+            for (String basePackage : packages)
+                for (BeanDefinition bd : scanner.findCandidateComponents(basePackage))
+                    register(ClassUtils.resolveClassName(bd.getBeanClassName(), getClass().getClassLoader()));
+        }
+
     }
 
     @Bean
