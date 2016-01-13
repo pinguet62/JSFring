a8e22c07e7104eb3bb4793182d0a030013748047
Pinguet62 <pinguet62@gmail.com>
2016-01-13 17:04:58 +0100

    Factorize access to resource

--- jsfring-common-test/src/test/java/frpinguet62/jsfring/util/FileCheckerTest.java
+++ jsfring-common-test/src/test/java/frpinguet62/jsfring/util/FileCheckerTest.java
@@ -41,235 +41,245 @@ import fr.pinguet62.jsfring.util.FileChecker;
  */
 public final class FileCheckerTest {
 
+    /**
+     * Get the resource file test from extension.
+     * 
+     * @param filename The extension, without {@code ""}.
+     * @return The {@link InputStream} of resource.
+     */
+    private InputStream getFileAsStream(String extension) {
+        return getClass().getResourceAsStream("/example." + extension);
+    }
+
     /** @see FileChecker#isCSV(InputStream) */
     @Test
     public void test_isCSV() {
-        String path = "/example.csv";
-        assertTrue(isCSV(getClass().getResourceAsStream(path)));
-        assertTrue(isTXT(getClass().getResourceAsStream(path)));
+        String path = "csv";
+        assertTrue(isCSV(getFileAsStream(path)));
+        assertTrue(isTXT(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isDOC(InputStream) */
     @Test
     public void test_isDOC() {
-        String path = "/example.doc";
-        assertTrue(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "doc";
+        assertTrue(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isDOCX(InputStream) */
     @Test
     public void test_isDOCX() {
-        String path = "/example.docx";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertTrue(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "docx";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertTrue(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isHTML(InputStream) */
     @Test
     public void test_isHTML() {
-        String path = "/example.html";
-        assertTrue(isHTML(getClass().getResourceAsStream(path)));
-        assertTrue(isTXT(getClass().getResourceAsStream(path)));
+        String path = "html";
+        assertTrue(isHTML(getFileAsStream(path)));
+        assertTrue(isTXT(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isImage(InputStream) */
     @Test
     public void test_isImage() {
-        assertTrue(isImage(getClass().getResourceAsStream("/example.jpg")));
-        assertTrue(isImage(getClass().getResourceAsStream("/example.png")));
-        assertTrue(isImage(getClass().getResourceAsStream("/example.svg")));
+        assertTrue(isImage(getFileAsStream("jpg")));
+        assertTrue(isImage(getFileAsStream("png")));
+        assertTrue(isImage(getFileAsStream("svg")));
     }
 
     /** @see FileChecker#isODP(InputStream) */
     @Test
     public void test_isODP() {
-        String path = "/example.odp";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertTrue(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "odp";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertTrue(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isODS(InputStream) */
     @Test
     public void test_isODS() {
-        String path = "/example.ods";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertTrue(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "ods";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertTrue(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isODT(InputStream) */
     @Test
     public void test_isODT() {
-        String path = "/example.odt";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertTrue(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "odt";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertTrue(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isPDF(InputStream) */
     @Test
     public void test_isPDF() {
-        assertTrue(isPDF(getClass().getResourceAsStream("/example.pdf")));
+        assertTrue(isPDF(getFileAsStream("pdf")));
     }
 
     /** @see FileChecker#isPPT(InputStream) */
     @Test
     public void test_isPPT() {
-        String path = "/example.ppt";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertTrue(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "ppt";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertTrue(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isPPTX(InputStream) */
     @Test
     public void test_isPPTX() {
-        String path = "/example.pptx";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertTrue(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "pptx";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertTrue(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isRTF(InputStream) */
     @Test
     public void test_isRTF() {
-        String path = "/example.rtf";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertTrue(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "rtf";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertTrue(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isTXT(getClass().getResourceAsStream(path)));
+        assertTrue(isTXT(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isTXT(InputStream) */
     @Test
     public void test_isTXT() {
-        assertTrue(isTXT(getClass().getResourceAsStream("/example.txt")));
+        assertTrue(isTXT(getFileAsStream("txt")));
     }
 
     /** @see FileChecker#isXLS(InputStream) */
     @Test
     public void test_isXLS() {
-        String path = "/example.xls";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertTrue(isXLS(getClass().getResourceAsStream(path)));
-        assertFalse(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "xls";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertTrue(isXLS(getFileAsStream(path)));
+        assertFalse(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isXLSX(InputStream) */
     @Test
     public void test_isXLSX() {
-        String path = "/example.xlsx";
-        assertFalse(isDOC(getClass().getResourceAsStream(path)));
-        assertFalse(isDOCX(getClass().getResourceAsStream(path)));
-        assertFalse(isODP(getClass().getResourceAsStream(path)));
-        assertFalse(isODS(getClass().getResourceAsStream(path)));
-        assertFalse(isODT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPT(getClass().getResourceAsStream(path)));
-        assertFalse(isPPTX(getClass().getResourceAsStream(path)));
-        assertFalse(isRTF(getClass().getResourceAsStream(path)));
-        assertFalse(isXLS(getClass().getResourceAsStream(path)));
-        assertTrue(isXLSX(getClass().getResourceAsStream(path)));
+        String path = "xlsx";
+        assertFalse(isDOC(getFileAsStream(path)));
+        assertFalse(isDOCX(getFileAsStream(path)));
+        assertFalse(isODP(getFileAsStream(path)));
+        assertFalse(isODS(getFileAsStream(path)));
+        assertFalse(isODT(getFileAsStream(path)));
+        assertFalse(isPPT(getFileAsStream(path)));
+        assertFalse(isPPTX(getFileAsStream(path)));
+        assertFalse(isRTF(getFileAsStream(path)));
+        assertFalse(isXLS(getFileAsStream(path)));
+        assertTrue(isXLSX(getFileAsStream(path)));
 
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isXML(InputStream) */
     @Test
     public void test_isXML() {
-        String path = "/example.xml";
-        assertTrue(isXML(getClass().getResourceAsStream(path)));
-        assertTrue(isTXT(getClass().getResourceAsStream(path)));
+        String path = "xml";
+        assertTrue(isXML(getFileAsStream(path)));
+        assertTrue(isTXT(getFileAsStream(path)));
     }
 
     /** @see FileChecker#isZIP(InputStream) */
     @Test
     public void test_isZIP() {
-        String path = "/example.zip";
-        assertTrue(isZIP(getClass().getResourceAsStream(path)));
+        String path = "zip";
+        assertTrue(isZIP(getFileAsStream(path)));
     }
 
 }
