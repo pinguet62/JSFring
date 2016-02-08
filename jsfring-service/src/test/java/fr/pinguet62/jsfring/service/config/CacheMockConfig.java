89e875318747015ad577cce750a3b8dbaafb2924
Pinguet62 <pinguet62@gmail.com>
2016-02-08 22:33:59 +0100

    Mock cache

---
+++ jsfring-service/src/test/java/fr/pinguet62/jsfring/service/config/CacheMockConfig.java
@@ -0,0 +1,18 @@
+package fr.pinguet62.jsfring.service.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.support.NoOpCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class CacheMockConfig {
+
+    @Bean
+    @Primary // Mock
+    public CacheManager r() {
+        return new NoOpCacheManager();
+    }
+
+}
