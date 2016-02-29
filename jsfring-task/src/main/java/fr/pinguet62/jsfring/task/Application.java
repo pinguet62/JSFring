41451c618e62a81dd74d2bd4accb26b8197c0907
Pinguet62 <pinguet62@gmail.com>
2016-02-29 19:02:50 +0100

    Migrate module config to main class

--- jsfring-task/src/main/java/fr/pinguet62/jsfring/task/Application.java
+++ jsfring-task/src/main/java/fr/pinguet62/jsfring/task/Application.java
@@ -1,9 +1,13 @@
 package fr.pinguet62.jsfring.task;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import fr.pinguet62.jsfring.SpringBootConfig;
 
+@Configuration
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
--- jsfring-task/src/main/java/fr/pinguet62/jsfring/task/config/TaskConfig.java
+++
@@ -1,8 +0,0 @@
-package fr.pinguet62.jsfring.task.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class TaskConfig {}
