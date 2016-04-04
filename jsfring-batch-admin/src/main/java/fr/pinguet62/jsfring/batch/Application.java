746f7451b0f846ada316c7b341125e7490d5b75e
Pinguet62 <pinguet62@gmail.com>
2016-04-04 21:01:36 +0200

    Fix batch config

--- jsfring-batch-admin/src/main/java/fr/pinguet62/jsfring/batch/Application.java
+++ jsfring-batch-admin/src/main/java/fr/pinguet62/jsfring/batch/Application.java
@@ -1,6 +1,5 @@
 package fr.pinguet62.jsfring.batch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,7 +8,6 @@ import fr.pinguet62.jsfring.SpringBootConfig;
 
 @Configuration
 @PropertySource("classpath:/application-batch-admin.properties")
-@EnableBatchProcessing
 public class Application {
 
     public static void main(String[] args) {
