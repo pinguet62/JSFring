13b33c7c2fe1ad0266afb1126f522ab7c658bff7
Pinguet62 <pinguet62@gmail.com>
2016-05-15 20:00:37 +0200

    OAuth: webservice to request authorities

--- jsfring-webservice/src/test/java/fr/pinguet62/jsfring/ws/ProfileWebserviceITTest.java
+++ jsfring-webservice/src/test/java/fr/pinguet62/jsfring/ws/ProfileWebserviceITTest.java
@@ -4,6 +4,7 @@ import static fr.pinguet62.jsfring.test.DbUnitConfig.DATASET;
 import static fr.pinguet62.jsfring.ws.Config.BASE_URL;
 import static fr.pinguet62.jsfring.ws.ProfileWebservice.PATH;
 import static fr.pinguet62.jsfring.ws.config.JerseyConfig.CONTEXT_PATH;
+import static fr.pinguet62.jsfring.ws.config.Oauth2Helper.HEADER_AUTHORIZATION;
 import static javax.ws.rs.client.ClientBuilder.newClient;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
@@ -29,6 +30,7 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 import fr.pinguet62.jsfring.SpringBootConfig;
 import fr.pinguet62.jsfring.dao.sql.ProfileDao;
 import fr.pinguet62.jsfring.model.sql.Profile;
+import fr.pinguet62.jsfring.ws.config.Oauth2Helper;
 import fr.pinguet62.jsfring.ws.dto.ProfileDto;
 
 /** @see ProfileWebservice */
@@ -57,7 +59,7 @@ public class ProfileWebserviceITTest {
                 .path(PATH)
                 .path("/{id}").resolveTemplate("id", id)
             .request()
-                .header("Authorization", helper.getAuthorization())
+                .header(HEADER_AUTHORIZATION, helper.getAuthorization())
             .get(ProfileDto.class);
         // @formatter:on
 
@@ -80,7 +82,7 @@ public class ProfileWebserviceITTest {
                 .path(PATH)
                 .path("/")
             .request()
-                .header("Authorization", helper.getAuthorization())
+                .header(HEADER_AUTHORIZATION, helper.getAuthorization())
             .get(new GenericType<List<ProfileDto>>() {});
         // @formatter:on
 
