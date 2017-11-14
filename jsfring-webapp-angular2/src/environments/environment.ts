1c0aa990be5272005da801339d0db18f2dc1068f
Pinguet62 <pinguet62@gmail.com>
2017-11-14 09:43:00 +0100

    Angular: CI config based on Maven

---
+++ jsfring-webapp-angular2/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {  
+    api: window.location.origin.replace('jsfring-webapp-angular2', 'jsfring-webservice'),
+    production: false
+};
--- jsfring-webapp-angular2/src/main/webapp/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {  
-    api: 'http://localhost:8080',
-    production: false
-};
