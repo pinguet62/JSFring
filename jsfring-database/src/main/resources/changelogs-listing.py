c8745eb96d55a1198feb6550b09d3e24c6e5cd47
Pinguet62 <pinguet62@gmail.com>
2017-12-12 15:01:32 +0100

    Liquibase: polish

--- jsfring-database/src/main/resources/changelogs-listing.py
+++ jsfring-database/src/main/resources/changelogs-listing.py
@@ -7,7 +7,7 @@ liquibaseFolder = os.path.dirname(sys.argv[0])
 os.chdir(liquibaseFolder)
 
 subChangelogs = sorted([filename for filename in os.listdir('changelogs') if re.match('.*\.yaml$', filename)])
-includes = ['  - include: { file: src/main/resources/changelogs/' + subChangelog + ' }\n' for subChangelog in subChangelogs]
+includes = ['  - include: { relativeToChangelogFile: true, file: changelogs/' + subChangelog + ' }\n' for subChangelog in subChangelogs]
 with open('changelog-master.yaml', 'w') as changelogFile:
     changelogFile.write('# Auto-generated file. Crushed during build.\n')
     changelogFile.write('databaseChangeLog:\n')
